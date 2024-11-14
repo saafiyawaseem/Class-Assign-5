c2399ca56968dc3f31af2ab0038253fbe5b85e9e
saafiyawaseem <mwaseemraza26@gmail.com>
2024-11-14 10:49:23 -0800

    main

---
+++ src/app/Component/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-[94px] bg-[#A29875]
+text-[#FFFFFF] font-normal  mt-5 pt-5 pl-2 text-4xl'>MANZZARI
+        
+      
+    </div>
+  )
+}
+
+export default Header
